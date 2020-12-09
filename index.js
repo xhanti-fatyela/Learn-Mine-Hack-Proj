58af09ada6fb449d5a38d35091a64a11970301df
xhanti-fatyela <xmanmrcool@gmail.com>
2020-12-09 10:21:57 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser')
+const flash = require('express-flash');
+const session = require('express-session');
+
+const app = express();
+
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+// initialise session middleware - flash-express depends on it
+app.use(session({
+  secret: "<add a secret string here>",
+  resave: false,
+  saveUninitialized: true
+}));
+
+// initialise the flash middleware
+app.use(flash());
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+app.get("/", function(req, res){
+  res.render('index');
+});
+
+let PORT = process.env.PORT || 2020;
+
+app.listen(PORT, function(){
+  console.log('App starting on port', PORT);
+});
