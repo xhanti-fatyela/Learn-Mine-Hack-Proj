a76bfb9caee0117384990d2667d4fb616b10711d
LuyoloLuja <luyolom19@gmail.com>
2020-12-10 18:11:15 +0200

    styled layout

--- public/checklevels.js
+++ public/checklevels.js
@@ -2,7 +2,7 @@
 // https://github.com/tensorflow/tfjs-models/tree/master/speech-commands
 
 // the link to your model provided by Teachable Machine export panel
-const URL = "https://teachablemachine.withgoogle.com/models/q6a1UP0Sq/";
+const URL = "https://teachablemachine.withgoogle.com/models/Yqd29C8IF/";
 // const URL = "https://teachablemachine.withgoogle.com/models/O8VsZEwdW/"
 async function createModel() {
     const checkpointURL = URL + "model.json"; // model topology
@@ -19,158 +19,6 @@ async function createModel() {
 
     return recognizer;
 }
-
-const levelOne = {
-    "greet_in_sotho" : false,
-    "greet_in_venda" : false,
-    "greet_in_xhosa" : false
-}
-
-const levelTwo = {
-    "have_a_nice_day_sesotho" : false,
-    "have_a_nice_day_venda" :  false,
-    "have_a_nice_day_xhosa" :  false
-};
-
-const levelThree = {
-    "how_are_you_sotho" : false,
-    "how_are_you_venda" : false,
-    "how_are_you_xhosa" : false
-};
-
-const levelsList = [levelOne, levelTwo, levelThree];
-
-let currentLevelIndex = 1;
-
-let currentLevel = levelsList[currentLevelIndex-1];
-
-let currentScore = 0;
-
-
-function nextLevel() {
-
-    currentLevelIndex++;
-    currentScore = 0;
-    currentLevel = levelsList[currentLevelIndex-1];
-    showNextLevel();
-
-}
-
-const keepPropability = [];
-// console.log(keepPropability);
-const checkScore = async () => {
-    // TO DO --- stop keepPropability from taking duplicates
-    
-    if (keepPropability.length === 3) {
-        return "done with this level";
-    } else {
-        return "Opps try again!!!";
-    }
-}
-
-async function init() {
-    const recognizer = await createModel();
-    const classLabels = recognizer.wordLabels(); // get class labels
-    const labelContainer = document.getElementById("label-container");
-    const scoreElement = document.querySelector(".counter");
-
-    // for (let i = 0; i < classLabels.length; i++) {
-    //     labelContainer.appendChild(document.createElement("div"));
-    // }
-
-    // listen() takes two arguments:
-    // 1. A callback function that is invoked anytime a word is recognized.
-    // 2. A configuration object with adjustable fields
-    recognizer.listen(async result => {
-        const scores = result.scores; // probability of prediction for each class
-
-
-        const labelsWithScore = classLabels.map(function(className, index){
-            return {
-                className,
-                score : result.scores[index] 
-            }
-        });
-
-        console.log(labelsWithScore);
-
-
-        const labelWithHighestScore = labelsWithScore.reduce(function(prev, current){
-            if (prev.score < current.score) {
-                return current;
-            }
-            return prev;
-        }, { className: "", score : 0 })
-
-        console.log(labelWithHighestScore);
-
-
-        if (labelWithHighestScore.score > 0.7 && currentLevel[labelWithHighestScore.className] !== undefined ) {
-
-            labelContainer.innerHTML = labelWithHighestScore.className;
-            currentScore++;
-
-            // playSound!
-
-            scoreElement.innerHTML = currentScore;
-
-            if (currentScore === 3) {
-                
-                nextLevel();
-
-            }
-            
-        } else {
-            // no that is not the correct word...
-            return "Not the correct word!"
-        }
-
-        // render the probability scores per class
-        // for (let i = 0; i < classLabels.length; i++) {
-        //     var classNames = classLabels[i];
-
-        //     // console.log(classNames);
-        //     var classPrediction = classLabels[i] + ": " + result.scores[i].toFixed(2);
-        //     // console.log(d === 'Background Noise' && result.scores[i].toFixed(2) > 0.7)
-
-        //     labelContainer.childNodes[i].innerHTML = classPrediction;
-        //     if (classNames === "greet_in_sotho" && result.scores[i].toFixed(2) > 0.7) {
-        //         keepPropability.push(result.scores[i].toFixed(2));
-        //     } else if (classNames === "greet_in_xhosa" && result.scores[i].toFixed(2) > 0.7) {
-        //         keepPropability.push(result.scores[i].toFixed(2));
-        //     } else if (classNames === "greet_in_venda" && result.scores[i].toFixed(2) > 0.7) {
-        //         keepPropability.push(result.scores[i].toFixed(2));
-        //     }
-        // }
-        
-    //    const checkScores = await checkScore();
-
-        // if (keep.includes(classNames)) {
-        //     return "already exists"
-        // }
-        // else {
-        // }
-        
-
-    }, {
-        includeSpectrogram: true, // in case listen should return result.spectrogram
-        probabilityThreshold: 0.75,
-        invokeCallbackOnNoiseAndUnknown: true,
-        overlapFactor: 0.50 // probably want between 0.5 and 0.75. More info in README
-    });
-
-
-    // Stop the recognition in 5 seconds.
-    // setTimeout(() => recognizer.stopListening(), 20000);
-}
-
-// DOM Code
-const nextBtn = document.querySelector(".nextBtn");
-const levels = document.querySelectorAll(".level");
-
-var showCounter = 0;
-
-
 /*
 
 greet_in_sotho: 0.03
@@ -183,41 +31,138 @@ how_are_you_sotho: 0.04
 how_are_you_venda: 0.00
 how_are_you_xhosa: 0.00
 
-
 */
+const levelOne = {
+    "greet_in_sotho": false,
+    "greet_in_venda": false,
+    "greet_in_xhosa": false
+}
+
+const levelTwo = {
+    "have_a_nice_day_sesotho": false,
+    "have_a_nice_day_venda": false,
+    "have_a_nice_day_xhosa": false
+};
+
+const levelThree = {
+    "how_are_you_sotho": false,
+    "how_are_you_venda": false,
+    "how_are_you_xhosa": false
+};
+
+const levelsList = [levelOne, levelTwo, levelThree];
+
+let currentLevelIndex = 1;
+
+let currentLevel = levelsList[currentLevelIndex - 1];
+
+let currentScore = 0;
+
+function nextLevel() {
+
+    currentLevelIndex++;
+    currentScore = 0;
+    currentLevel = levelsList[currentLevelIndex - 1];
+    showNextLevel();
+
+}
+// sound function
+// var mySound;
+// function sound(src) {
+//     this.sound = document.createElement("audio");
+//     this.sound.src = src;
+//     this.sound.setAttribute("controls", "none");
+//     this.sound.style.display = "none";
+//     document.body.appendChild(this.sound);
+//     this.play = function () {
+//         this.sound.play();
+//     }
+//     this.stop = function () {
+//         this.sound.pause();
+//     }
+//}
+async function init() {
+    const recognizer = await createModel();
+    const classLabels = recognizer.wordLabels(); // get class labels
+    const labelContainer = document.getElementById("label-container");
+    const scoreElement = document.querySelector(".counter");
+    const errorElement = document.querySelector(".error");
+    // listen() takes two arguments:
+    // 1. A callback function that is invoked anytime a word is recognized.
+    // 2. A configuration object with adjustable fields
+    recognizer.listen(async result => {
+        const scores = result.scores; // probability of prediction for each class
+
+
+        const labelsWithScore = classLabels.map(function (className, index) {
+            return {
+                className,
+                score: result.scores[index]
+            }
+        });
+        console.log(labelsWithScore);
+
+        const labelWithHighestScore = labelsWithScore.reduce(function (prev, current) {
+            if (prev.score < current.score) {
+                return current;
+            }
+            return prev;
+        }, { className: "", score: 0 })
+
+        console.log(labelWithHighestScore);
+
+
+        if (labelWithHighestScore.score > 0.7 && currentLevel[labelWithHighestScore.className] !== undefined) {
+
+            labelContainer.innerHTML = labelWithHighestScore.className;
+            currentScore++;
+
+            // playSound!
+            // mySound = new sound("sound.mp3");
+            scoreElement.innerHTML = currentScore;
+            const success = new Audio('/audio/sound.mp3');
+
+            if (currentScore === 3) {
+                success.play();
+                nextLevel();
+
+            }
+
+        } else {
+            // no that is not the correct word...
+            errorElement.innerHTML = "Not a correct word!";
+        }
+    }, {
+        includeSpectrogram: true, // in case listen should return result.spectrogram
+        probabilityThreshold: 0.75,
+        invokeCallbackOnNoiseAndUnknown: true,
+        overlapFactor: 0.50 // probably want between 0.5 and 0.75. More info in README
+    });
+    // Stop the recognition in 5 seconds.
+    // setTimeout(() => recognizer.stopListening(), 20000);
+}
+
+// DOM Code
+const nextBtn = document.querySelector(".nextBtn");
+const levels = document.querySelectorAll(".level");
+
+var showCounter = 0;
 
 function showNextLevel() {
     if (showCounter >= 4) {
-
         return;
     }
 
     if (showCounter > 0) {
-        levels[showCounter-1].classList.add("hidden");
+        levels[showCounter - 1].classList.add("hidden");
     }
 
     levels[showCounter].classList.remove("hidden");
     showCounter++;
 }
 
-nextBtn.addEventListener("click", function(){
-    
+nextBtn.addEventListener("click", function () {
 
     showNextLevel();
-
-    // if (showCounter >= 4) {
-
-    //     return;
-    // }
-
-    // if (showCounter > 0) {
-    //     levels[showCounter-1].classList.add("hidden");
-    // }
-
-    // levels[showCounter].classList.remove("hidden");
-    // showCounter++;
-
-    // alert(levels[1].classList.contains("hidden"));
-    // alert(levels[2].classList.contains("hidden"));
 
 });
